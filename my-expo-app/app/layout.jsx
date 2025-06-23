367e456308afd4150f33a492043e9a5fe35823ee
tarun <r.tarunnayaka25042005@gmail.com>
2025-06-23 13:54:37 +0530

    refactor: wrap Slot component in SafeAreaView for improved layout handling

--- my-expo-app/app/layout.jsx
+++ my-expo-app/app/layout.jsx
@@ -2,11 +2,14 @@ import { View } from 'react-native';
 import React from 'react';
 import { Slot } from 'expo-router';
 import '../global.css';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const layout = () => {
   return (
     <View className="flex-1">
-      <Slot />
+      <SafeAreaView>
+        <Slot />
+      </SafeAreaView>
     </View>
   );
 };
